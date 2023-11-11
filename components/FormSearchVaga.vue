03e263422b514615d93c522563773632d06b6c2c
henricosta <pedrocosta.lov@gmail.com>
2023-11-11 20:33:26 -0300

    add filters tab for vaga search

--- components/FormSearchVaga.vue
+++ components/FormSearchVaga.vue
@@ -6,5 +6,14 @@
                 <button type="submit" class="px-4 py-4 rounded-l-none border bg-black text-white">Buscar vaga</button>
             </div>
         </div>
+        <div class="border-t shadow-sm bg-gray-50 py-4 flex justify-center">
+            <button class="rounded-full border border-gray-800 hover:bg-gray-700 text-white bg-gray-800 px-5 mr-5">Redefinir filtros</button>
+            <select class="py-2 mx-2 border">
+                <option value="0" disabled selected>Cidade</option>
+            </select>
+            <select class="py-2 mx-2 border">
+                <option value="0" disabled selected>Modalidade</option>
+            </select>
+        </div>
     </form>
 </template>
