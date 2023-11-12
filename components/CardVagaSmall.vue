c29ae6c2217f8c6a2afff06a94b55994e69036e4
henricosta <pedrocosta.lov@gmail.com>
2023-11-12 12:52:35 -0300

    add component for small display of vagas

---
+++ components/CardVagaSmall.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="mb-3 border rounded-md shadow-sm p-4 w-72 cursor-pointer hover:bg-gray-50">
+        <h1 class="text-lg"><strong>Titulo da vaga</strong></h1>
+        <p class="text-sm text-gray-600">empresa - (modalidade)</p>
+        <p class="mt-2 text-sm text-gray-600">descricao da vaga</p>
+    </div>
+</template>
