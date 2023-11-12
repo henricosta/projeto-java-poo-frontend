4fb66ffcaef17561cc03a2100e0d94609417b45f
henricosta <pedrocosta.lov@gmail.com>
2023-11-12 10:46:25 -0300

    create component to display selected vaga info

---
+++ components/PageVaga.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class=" bg-white shadow-sm mx-6 border rounded-lg mt-3">
+        <div class="m-14">
+            <h2 class="text-3xl mb-2">Titulo da vaga</h2>
+            <p class="text-gray-800">Empresa - (modalidade)</p>
+            <button class="px-2 py-3 mt-4">Candidatar-se</button>
+            <p class="mt-5"><strong>Descrição da vaga</strong></p>
+            <hr class="my-1">
+            <p id="descricao-vaga" class="descricao">Descricao da vaga</p>
+        </div>
+    </div>
+</template>
