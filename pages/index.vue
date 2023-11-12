34a79df48758d1f1e4904e0036b68471dfd42e5e
henricosta <pedrocosta.lov@gmail.com>
2023-11-12 10:46:47 -0300

    add vagas to index layout

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,12 @@ definePageMeta({
         <div>
             <FormSearchVaga />
         </div>
+        <div class="flex justify-center">
+            <CardVaga class="w-1/3"/>
+            <div class="w-1/2">
+                <PageVaga />
+            </div>
+        </div>
     </div>
 </template>
   
