6b07c3210c50a221a098a4b45ba97f8e811993f9
henricosta <pedrocosta.lov@gmail.com>
2023-11-25 10:39:30 -0300

    implement post request to create candidate

--- pages/register/candidato.vue
+++ pages/register/candidato.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
+import axios from 'axios';
+
 definePageMeta({
   layout: 'guest'
 })
+
+const { apiBase } = useRuntimeConfig().public;
+
+const name = ref('')
+const email = ref('')
+const password = ref('')
+
+async function postCandidateRegister() {
+  console.log(name, email, password)
+  const response = await axios.post(apiBase + '/auth/register/candidate', {
+    name: name.value,
+    email: email.value,
+    password: password.value
+  })
+
+  console.log(response)
+}
 </script>
 
 <template>
@@ -11,23 +30,23 @@ definePageMeta({
       <form @submit.prevent>
         <div>
           <label for="name">Nome</label>
-          <input id="name" type="text" class="mt-1 block w-full border">
+          <input v-model="name" id="name" type="text" class="mt-1 block w-full border">
         </div>
 
         <div>
           <label for="email">Email</label>
-          <input id="email" type="text" class="mt-1 block w-full border">
+          <input v-model="email" id="email" type="text" class="mt-1 block w-full border">
         </div>
 
         <div>
           <label for="password">Senha</label>
-          <input id="password" type="text" class="mt-1 block w-full border">
+          <input v-model="password" id="password" type="password" class="mt-1 block w-full border">
         </div>
 
         <div class="flex items-center justify-end mt-4">
           <NuxtLink to="/login/candidato"
             class="ml-4 bg-gray-100 px-2 py-1 rounded-md border text-black hover:bg-gray-200 ">Já tem conta?</NuxtLink>
-          <button class="ml-4">
+          <button class="ml-4" @click="postCandidateRegister">
             Criar conta
           </button>
         </div>
