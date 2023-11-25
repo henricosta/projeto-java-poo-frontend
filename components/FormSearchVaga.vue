caac7c9840fcf78205e5cdfeee9f581b4bdfa036
henricosta <pedrocosta.lov@gmail.com>
2023-11-25 10:28:08 -0300

    use prop funcion to update list of jobs

--- components/FormSearchVaga.vue
+++ components/FormSearchVaga.vue
@@ -1,9 +1,18 @@
+<script setup>
+defineProps({
+    searchJobsFunction: Function
+})
+const title = ref('')
+const type = ref('')
+
+</script>
+
 <template>
     <form @submit.prevent id="form-busca-vaga">
         <div class="bg-gray-50 flex justify-center">
             <div class="py-8 flex">
-                <input type="text" placeholder="Ex: Desenvolvedor web" class="border w-96 rounded-r-none px-4"/>
-                <button type="submit" class="px-4 py-4 rounded-l-none border bg-black text-white">Buscar vaga</button>
+                <input v-model="title" type="text" placeholder="Ex: Desenvolvedor web" class="border w-96 rounded-r-none px-4"/>
+                <button @click="() => searchJobsFunction(title, type)" class="px-4 py-4 rounded-l-none border bg-black text-white">Buscar vaga</button>
             </div>
         </div>
         <div class="border-t shadow-sm bg-gray-50 py-4 flex justify-center">
@@ -11,8 +20,10 @@
             <select class="py-2 mx-2 border px-2 rounded-full">
                 <option value="0" disabled selected>Cidade</option>
             </select>
-            <select class="py-2 mx-2 border px-2 rounded-full">
-                <option value="0" disabled selected>Modalidade</option>
+            <select @change="() => searchJobsFunction(title, type)" v-model="type" class="py-2 mx-2 border px-2 rounded-full">
+                <option value="Modalidade" disabled selected>Modalidade</option>
+                <option value="Presencial">Presencial</option>
+                <option value="Remoto">Remoto</option>
             </select>
         </div>
     </form>
