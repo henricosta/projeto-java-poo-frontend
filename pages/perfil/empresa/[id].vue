67a88e1c86287ffdfe916f4a51b41b284bb3894a
henricosta <pedrocosta.lov@gmail.com>
2023-11-26 11:04:40 -0300

    retrive company info from api

--- pages/perfil/empresa/[id].vue
+++ pages/perfil/empresa/[id].vue
@@ -1,14 +1,29 @@
 <script setup>
+import axios from 'axios';
+
+// TODO: Mudar layout
 definePageMeta({
-    layout: 'candidato'
+  layout: 'guest'
 })
+
+const { apiBase } = useRuntimeConfig().public
 const route = useRoute()
-const { data: empresa } = await useFetch("http://localhost:8080/empresas/" + route.params.id + "/profile")
+
+
+const company = ref({})
+const { data } = await axios.get(apiBase + '/companies/details', {
+  params: {
+    company_id: route.params.id
+  }
+})
+company.value = data
+// const { data: empresa } = await useFetch("http://localhost:8080/empresas/" + route.params.id + "/profile")
 </script>
 
 <template>
   <div>
     <Section>
+      {{ console.log(company) }}
       <div
         class="mb-8 relative inline-flex items-center justify-center w-60 h-60 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
         <svg class="absolute w-full h-full text-gray-400 mt-12" fill="currentColor" viewBox="0 0 20 20"
@@ -17,12 +32,12 @@ const { data: empresa } = await useFetch("http://localhost:8080/empresas/" + rou
         </svg>
       </div>
       <div class="flex items-center">
-        <div class="mb-3 text-4xl font-extrabold dark:text-white">{{ empresa.name }}</div>
+        <div class="mb-3 text-4xl font-extrabold dark:text-white">{{ company.name }}</div>
         <button class="ml-4 mb-2 py-2 px-3 rounded-md border bg-gray-200 shadow-sm text-sm hover:bg-gray-400">Editar
           informações</button>
       </div>
       <ul class="max-w-md space-y-1 text-gray-500 list-none list-inside dark:text-gray-400">
-        <li>{{ empresa.email }}</li>
+        <li>{{ company.email }}</li>
       </ul>
     </Section>
     <Section class="mr-8">
@@ -32,7 +47,7 @@ const { data: empresa } = await useFetch("http://localhost:8080/empresas/" + rou
           vaga</button>
       </div>
       <hr class="mt-2 mb-3">
-      <CardVagaSmall />
+      <!-- <CardVagaSmall /> -->
     </Section>
   </div>
 </template>
